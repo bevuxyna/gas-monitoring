fe24c4a21cfa2756c12a6fa8e96ac4b342be521a
Vlada Melnikova <wannhorn@gmail.com>
2026-03-09 16:02:53 +0300

    fix header (#3)

---
+++ src/themes/index.ts
@@ -0,0 +1,35 @@
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/Button' {
+    interface ButtonPropsVariantOverrides {
+        primary: true;
+        secondary: true;
+        danger: true;
+        outline: true;
+    }
+}
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#333b4d',
+        },
+        secondary: {
+            main: '#203bc5',
+        },
+    },
+
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: '6px',
+                    textTransform: 'none',
+                    fontWeight: 600,
+                    padding: '6px 12px',
+                    transition: 'all 0.3s ease',
+                },
+            },
+        },
+    },
+});
